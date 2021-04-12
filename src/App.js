7c2b419b3c48dd9e11c1443c1fe5ef407a7fd647
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-04-12 17:07:51 +0200

    Added routing to the logo navbar

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import About from './pages/about/About.js';
 import Catalog from './pages/catalog/Catalog.js';
 import Commission from './pages/commission/Commission.js';
 import Contact from './pages/contact/Contact.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Logo />
-      <About />
-      <Catalog />
-      <Commission/>
-      <Contact />
-    </div>
+    <Router >
+      <div className="App">
+        <Logo />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/catalog" component={Catalog} />
+          <Route path="/commissions" component={Commission} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
