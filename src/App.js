21e0c10e90783e9f5b81868f7273358f99313134
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-04-12 14:23:53 +0200

    Test component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>This will be the furniture app homepage</h1>
+        <Test />
       </header>
     </div>
   );
---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      <h2>Test text</h2>
+    </div>
+  )
+}
+
+export default Test
