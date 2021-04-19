fbb155bd7d95fc3ae4fc70c2d3292027d80edf6a
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-04-19 15:39:37 +0100

    Installed react-player and have placeholder video on about page. Added placeholder text to about page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Video from '../../components/video/Video';
 import './about.css';
 
 const About = () => {
   return (
-    <div className="about-border">
-      <h1>About / welcome... </h1>
-      <p>Introduction page</p>
-    </div>
+    <section className="about">
+      {/* <h1>About / welcome... </h1> */}
+      <div className="about__txt">
+        <p><strong>Brand name</strong> is a Stockholm based carpenter that produces unique, handcrafted pieces.</p>
+        <br></br>
+        <p>Brand name produces seasonal collections but is also available for commissions, simply contact them <Link to="/commissions"><button className="btn"><strong>here</strong></button></Link></p>
+        <br></br>
+        <p>Click <Link to="/catalog"><button className="btn"><strong>here</strong></button></Link> to view brand name's collection.</p>
+        <Video />
+      </div>
+    </section>
   );
 };
 
