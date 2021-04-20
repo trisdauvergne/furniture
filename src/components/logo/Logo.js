c6e8cd5b547e30f3fc2430b32e19a38c3f5e0a24
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-04-20 12:30:12 +0100

    Changed menu from modal to dropdown and updated responsiveness in nav bar to center logo and hide menu button

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -48,7 +48,23 @@ export const Logo = () => {
   return (
     <nav className="nav">
       <h1 className="nav__logo">Brand name</h1>
-      {!dropdownOpen && <button  className="btn nav__btn" onClick={changeDropdown}>Menu</button>}
+      <button className={dropdownOpen ? 'btn nav__btn nav__btn--bold' : 'btn nav__btn'} onClick={changeDropdown}>Menu</button>
+      <div className="nav__menu">
+      {dropdownOpen && <>
+        <Link to="/about">
+          <button className="btn" onClick={changeDropdown}>About</button>
+        </Link>
+        <Link to="/catalog">
+          <button className="btn" onClick={changeDropdown}>Catalog</button>
+        </Link>
+        <Link to="/commissions">
+          <button className="btn" onClick={changeDropdown}>Commissions</button>
+        </Link>
+        <Link to="/contact">
+          <button className="btn" onClick={changeDropdown}>Contact</button>
+        </Link></>}
+      </div>
+      {/* {!dropdownOpen && <button  className="btn nav__btn" onClick={changeDropdown}>Menu</button>}
       <ModalDropdown open={dropdownOpen} onClose={() => setDropdownOpen(false)}>
         <div className="nav__menu">
           <Link to="/about">
@@ -64,7 +80,7 @@ export const Logo = () => {
             <button className="btn" onClick={changeDropdown}>Contact</button>
           </Link>
         </div>
-      </ModalDropdown>
+      </ModalDropdown> */}
     </nav>
   );
 };
